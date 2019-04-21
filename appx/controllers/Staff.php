e557a865c343643e9bd16efb650a191c08858e4a
zatilimaniahwan <zatilimaniahwan@gmail.com>
2019-04-22 02:35:21 +0800

    update

--- appx/controllers/Staff.php
+++ appx/controllers/Staff.php
@@ -1,25 +1,27 @@
 <?php
 
 require(APPPATH.'/libraries/REST_Controller.php');
+require(APPPATH.'/libraries/Bcrypt.php');
  
-class Recipient extends REST_Controller{
+class Staff extends REST_Controller{
     
     public function __construct()
     {
         
         parent::__construct();
 
-        $this->load->model('recipient_model');
+        $this->load->model('staff_model');
+        $this->load->library('bcrypt');
     }
 
-    //API - client sends id and on valid id recipient information is sent back
-    function recipientById_get(){
+    //API - client sends id and on valid id staff information is sent back
+    function staffById_get(){
         $id  = $this->get('id');
         if(!$id){
             $this->response("No ID specified", 400);
             exit;
         }
-        $result = $this->recipient_model->getrecipientbyid( $id );
+        $result = $this->staff_model->getstaffbyid( $id );
         if($result){
             $this->response($result, 200); 
             exit;
@@ -30,9 +32,9 @@ class Recipient extends REST_Controller{
         }
     } 
 
-    //API -  Fetch All Recipients
-    function recipients_get(){
-        $result = $this->recipient_model->getallrecipients();
+    //API -  Fetch All staffs
+    function staffs_get(){
+        $result = $this->staff_model->getallstaffs();
         if($result){
             $this->response($result, 200); 
         } 
@@ -41,33 +43,29 @@ class Recipient extends REST_Controller{
            }
     }
      
-    //API - create a new usergroup item in database.
-    function addRecipient_post(){
+    //API - create a new staff item in database.
+    function addStaff_post(){
         $now = date('Y-m-d H:i:s');
         // decode input from angular. The form of input is a json encoded
         $json = json_decode(file_get_contents('php://input'),true);
         //check the content of array
-        var_dump($json);
-        $fullname="";
-        if(!empty($json['organization_code'])&&!empty($json['fullname'])&&!empty($json['ic'])&&!empty($json['address'])&&!empty($json['state'])&&!empty($json['age'])&&!empty($json['tel_no'])&&!empty($json['family_no'])&&!empty($json['income'])){
-           $fullname=$json['fullname'];
-           $ic_no=$json['ic'];
-           $address=$json['address'];
-           $state_id=$json['state'];
-           $age=$json['age'];
-           $income=$json['income'];
-           $phone_no=$json['tel_no'];
-           $no_family=$json['family_no']; 
-           $organization_code=$json['organization_code'];
+        if(!empty($json['organization_code'])&&!empty($json['staff_id'])&&!empty($json['fullname'])&&!empty($json['password'])&&!empty($json['email'])&&!empty($json['usergroup'])){
+         $organization_code=$json['organization_code'];
+         $staff_id=$json['staff_id'];
+         $fullname=$json['fullname'];
+         $password=$json['password'];
+         $pass_hash=$this->bcrypt->hash_password($password);
+         $email=$json['email'];
+         $usergroup=$json['usergroup'];
         }
          $created_dt=$now;
          $created_by="admin";
-         if(!$fullname || !$ic_no || !$address || !$state_id || !$age || !$income || !$phone_no || !$no_family){
-                $this->response("Enter complete recipient information to save", 400);
+         if(!$organization_code||!$staff_id||!$fullname||!$pass_hash||!$email||!$usergroup){
+                $this->response("Enter complete staff information to save", 400);
          }else{
-            $result = $this->recipient_model->add(array("organisation_code"=>$organization_code, "fullname"=>$fullname,"ic_no"=>$ic_no,"address"=>$address,"state_id"=>$state_id,"age"=>$age,"phone_no"=>$phone_no,"no_family"=>$no_family,"income"=>$income,"status"=>1,"created_dt"=>$created_dt,"created_by"=>$created_by));
+            $result = $this->staff_model->add(array("organization_code"=>$organization_code, "staff_code"=>$staff_id,"fullname"=>$fullname,"password"=>$pass_hash,"email"=>$email,"usergroup"=>$usergroup,"created_dt"=>$created_dt,"created_by"=>$created_by));
             if($result === 0){
-                $this->response("Recipient information could not be saved. Try again.", 404);
+                $this->response("Staff information could not be saved. Try again.", 404);
             }else{
                 $this->response("success", 200);  
             }
--- appx/models/Staff_model.php
+++ appx/models/Staff_model.php
@@ -2,107 +2,65 @@
   class Staff_model extends CI_Model {
        
       public function __construct(){
-          
         $this->load->database();
-        
       }
       
-      //API call - get a usergroup record by id
-      public function getorganizationbyid($id){  
-
-           $this->db->select('id,');
-
-           $this->db->from('sfb_organizations');
-
+      //API call - get a staff record by id
+      public function getstaffbyid($id){  
+           $this->db->select('id,org_code,staff_code,fullname,password,email,usergroup');
+           $this->db->from('sfb_staffs');
            $this->db->where('id',$id);
-
            $query = $this->db->get();
-           
            if($query->num_rows() == 1)
            {
-
                return $query->result_array();
-
            }
            else
            {
-
              return 0;
-
           }
-
       }
 
-    //API call - get all usergroups record
-    public function getallorganizations(){   
-
-      $this->db->select('id,reg_no,code,org_name,address,state_id,email,tel_no,fax_no');
-
-        $this->db->from('sfb_organizations');
-
-        $this->db->order_by("created_dt", "desc"); 
-
+    //API call - get all staf record
+    public function getallstaffs(){   
+      $this->db->select('org_name,staff_code,fullname');
+        $this->db->from('sfb_staffs');
+        $this->db->join('sfb_organizations','sfb_organizations.code=sfb_staffs.organization_code','inner');
+        $this->db->order_by('sfb_staffs.created_dt', "desc"); 
         $query = $this->db->get();
-
         if($query->num_rows() > 0){
-
           return $query->result_array();
-
         }else{
-
           return 0;
-
         }
-
     }
    
-   //API call - delete a usergroup record
+   //API call - delete a staff record
     public function delete($id){
-
        $this->db->where('id', $id);
-
-       if($this->db->delete('sfb_organizations')){
-
+       if($this->db->delete('sfb_staffs')){
           return true;
-
         }else{
-
           return false;
-
         }
-
    }
    
-   //API call - add new usergroup record
+   //API call - add new staff record
     public function add($data){
-
-        if($this->db->insert('sfb_organizations', $data)){
-
+        if($this->db->insert('sfb_staffs', $data)){
            return true;
-
         }else{
-
            return false;
-
         }
-
     }
     
-    //API call - update a usergroup record
+    //API call - update a staff record
     public function update($id, $data){
-
        $this->db->where('id', $id);
-
-       if($this->db->update('sfb_organizations', $data)){
-
+       if($this->db->update('sfb_staffs', $data)){
           return true;
-
         }else{
-
           return false;
-
         }
-
     }
-
 }
